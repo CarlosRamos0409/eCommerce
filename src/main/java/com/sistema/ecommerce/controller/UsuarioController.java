41a801ae87d1d965e09e1b657b50839d43285cd7
CarlosRamos0409 <151487579+ipmontecristo@users.noreply.github.com>
2024-02-11 17:16:46 -0300

    implemento la vista de las compras

--- src/main/java/com/sistema/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/sistema/ecommerce/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -86,4 +87,17 @@ public class UsuarioController {
 		return "usuario/compras";
 	}
 	
+	
+	@GetMapping("/detalle/{id}")
+	public String detalleCompras(@PathVariable Integer id, HttpSession session, Model model) {
+		logger.info("Id de la Orden: {}", id);
+		Optional<Orden> orden=ordenService.findById(id);
+		
+		model.addAttribute("detalles", orden.get().getDetalle());
+		
+		//sesion
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		
+		return "usuario/detallecompra";
+	}
 }
