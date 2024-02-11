8623d0ed3dd7a50ab52f66cdfca32120ac894c2b
CarlosRamos0409 <151487579+ipmontecristo@users.noreply.github.com>
2024-02-11 16:26:42 -0300

    muestra la vista compras en cada usuario

--- src/main/java/com/sistema/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/sistema/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,4 +70,18 @@ public class UsuarioController {
 		
 		return "redirect:/";
 	}
+	
+	@GetMapping("/compras")
+	public String obtenerCompras(Model model, HttpSession session) {
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
+		/*
+		Usuario usuario= usuarioService.findById(  Integer.parseInt(session.getAttribute("idusuario").toString()) ).get();
+		List<Orden> ordenes= ordenService.findByUsuario(usuario);
+		logger.info("ordenes {}", ordenes);
+		
+		model.addAttribute("ordenes", ordenes);
+		*/
+		return "usuario/compras";
+	}
+	
 }
